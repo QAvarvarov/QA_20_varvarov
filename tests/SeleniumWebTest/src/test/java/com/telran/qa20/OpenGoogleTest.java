704a420f4fdbd52bde3ca0458fa29b1a9561cecf
Anton <972varvarov@gmail.com>
2019-06-21 11:48:25 +0300

    21/06/2019

---
+++ tests/SeleniumWebTest/src/test/java/com/telran/qa20/OpenGoogleTest.java
@@ -0,0 +1,36 @@
+package com.telran.qa20;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OpenGoogleTest
+{
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+    }
+
+    @Test
+    public void testGoogle()
+    {
+        System.out.println("Google is open");//sout
+    }
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
+
+    }
+
+
+
+
+
+}

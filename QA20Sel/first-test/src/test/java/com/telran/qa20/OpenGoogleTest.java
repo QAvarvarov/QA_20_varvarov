5a4e7f0063f0a7ed47351a047ad065048ec050f3
Anton <972varvarov@gmail.com>
2019-06-25 19:52:19 +0300

    25/06/2019

--- QA20Sel/first-test/src/test/java/com/telran/qa20/OpenGoogleTest.java
+++ QA20Sel/first-test/src/test/java/com/telran/qa20/OpenGoogleTest.java
@@ -2,12 +2,11 @@ package com.telran.qa20;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterMethod;
 
-public class OpenGoogleTest
-{
+public class OpenGoogleTest {
     WebDriver driver;
     @BeforeMethod
     public void setUp()
